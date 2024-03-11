9d0447654ef709c4ae7c6773896692a53cb566a3
Pracma16 <oportunilan@gmail.com>
2024-03-11 00:38:35 -0300

    Responsi terminado

--- js/style.js
+++ js/style.js
@@ -1,3 +1,4 @@
+/*Efecto parallax*/
 $(document).ready(function() {
     $(window).scroll(function() {
         var scrollPosition = $(window).scrollTop();
@@ -8,15 +9,6 @@ $(document).ready(function() {
 window.onload = function(){
     $('#onload').fadeOut(); 
 }
-
-
-/*import ReactDOM from "https://esm.sh/react-dom@18.2.0/client"
-
-const appDomElement = document.getElementById('app')
-
-const root = ReactDOM.createRoot(appDomElement)
-root.render('Hola que tal')*/
-
 
 // Mostrar el botón cuando el usuario se desplace hacia abajo
 window.onscroll = function() {scrollFunction()};
@@ -33,4 +25,41 @@ function scrollFunction() {
 document.getElementById('backToTop').addEventListener('click', function(event) {
     event.preventDefault();
     window.scrollTo({top: 0, behavior: 'smooth'});
+});
+
+/*Boton Hamburguesa */
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+const navLinks = document.querySelectorAll(".nav__link");
+
+abrir.addEventListener("click", () =>{
+    nav.classList.add("visible");
+});
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+});
+
+// Ocultar el menú al hacer clic fuera de él
+document.addEventListener("click", (e) => {
+    if (!nav.contains(e.target) && !abrir.contains(e.target)) {
+        nav.classList.remove("visible");
+    }
+});
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("visible");
+    });
+});
+
+/*Calificacion con estrellas*/
+
+document.querySelectorAll('.rating').forEach(rating => {
+    rating.addEventListener('change', (e) => {
+        const ratingValue = e.target.value;
+        const ratingId = rating.dataset.ratingId;
+        console.log(`Rating for ${ratingId}: ${ratingValue}`);
+    });
 });
